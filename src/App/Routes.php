f7a9be1c53f94937431f2669a0154b5788b7a57b
DeMesmaekerIndra <indra.demesmaeker@student.odisee.be>
2020-05-26 19:08:29 +0200

    Update Routes.php

--- src/App/Routes.php
+++ src/App/Routes.php
@@ -3,8 +3,20 @@
 declare (strict_types = 1);
 
 use Slim\Routing\RouteCollectorProxy;
-$app->group('/opo', function (RouteCollectorproxy $group) {
-    $group->get('/{id}', 'App\Controller\OpoController:get');
-    $group->get('/', 'App\Controller\OpoController:getAll');
 
+$app->group('/opo', function (RouteCollectorproxy $group) {
+    $group->get('/', 'App\Controller\OpoController:getAll');
+    $group->get('/{id}', 'App\Controller\OpoController:get');
+});
+
+$app->group('/ola', function (RouteCollectorProxy $group) {
+    $group->get('/', 'App\Controller\OpoController:getAll');
+    $group->get('/{id}', 'App\Controller\OpoController:get');
+    $group->get('/allByOpo/{OpoId}', 'App\Controller\OpoController:get');
+});
+
+$app->group('/Personeel', function (RouteCollectorproxy $group) {
+    $group->get('/', 'App\Controller\OpoController:getAll');
+    $group->get('/{id}', 'App\Controller\OpoController:get');
+    $group->get('/allByOla/{olaId}', 'App\Controller\OpoController:get');
 });
