f4012f17e27e518a0e2da284a72a8bf5b843b7b4
DeMesmaekerIndra <indra.demesmaeker@student.odisee.be>
2020-06-14 18:05:14 +0200

    Fix forced stringify of DB values

--- src/App/Dependencies.php
+++ src/App/Dependencies.php
@@ -12,6 +12,7 @@ $container['pdo'] = static function (Container $c): PDO {
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false,
+        PDO::ATTR_STRINGIFY_FETCHES => false,
         PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
 
     try {
--- src/Factory/ResponseFactory.php
+++ src/Factory/ResponseFactory.php
@@ -11,9 +11,7 @@ final class ResponseFactory
     public function buildOKResponse($data): Response
     {
         $response = new Response();
-
-        $parsedBody = getJsonObjFromResult($data);
-        $body = json_encode(['data' => $parsedBody]);
+        $body = json_encode(['data' => $data]);
 
         $response->getBody()->write($body);
         $response->withProtocolVersion('2.0');
@@ -24,7 +22,7 @@ final class ResponseFactory
     public function buildOKResponseWithMessage($message): Response
     {
         $response = new Response();
-        $body = json_encode(['message' => $message]);
+        $body = json_encode(['data' => $data]);
 
         $response->getBody()->write($body);
         $response->withProtocolVersion('2.0');
@@ -35,9 +33,7 @@ final class ResponseFactory
     public function buildOKResponseWithDataAndMessage($data, $message): Response
     {
         $response = new Response();
-
-        $parsedBody = getJsonObjFromResult($data);
-        $body = json_encode(['data' => $parsedBody, 'message' => $message]);
+        $body = json_encode(['data' => $data, 'message' => $message]);
 
         $response->getBody()->write($body);
         $response->withProtocolVersion('2.0');
@@ -75,44 +71,10 @@ final class ResponseFactory
     public function buildNotAllowedResponse(): Response
     {
         $response = new Response();
-        $body = json_encode(['Message' => 'You are not authorized to access to access this endpoint or data!']);
-        $response->getBody()->write($body);
+        $body = ['Message' => 'You are not authorized to access to access this endpoint or data!'];
+        $response->getBody()->write(json_encode($body));
         $response->withProtocolVersion('2.0');
         $response->withStatus(405);
         return $response;
-    }
-
-    private function getJsonObjFromResult(&$result)
-    {
-        $fixed = array();
-
-        $typeArray = array(
-            MYSQLI_TYPE_TINY, MYSQLI_TYPE_SHORT, MYSQLI_TYPE_INT24,
-            MYSQLI_TYPE_LONG, MYSQLI_TYPE_LONGLONG,
-            MYSQLI_TYPE_DECIMAL,
-            MYSQLI_TYPE_FLOAT, MYSQLI_TYPE_DOUBLE);
-        $fieldList = array();
-
-        while ($info = $result->fetch_field()) {
-            $fieldList[] = $info;
-        }
-
-        while ($row = $result->fetch_assoc()) {
-            $fixedRow = array();
-            $teller = 0;
-
-            foreach ($row as $key => $value) {
-
-                if (in_array($fieldList[$teller]->type, $typeArray)) {
-                    $fixedRow[$key] = 0 + $value;
-                } else {
-                    $fixedRow[$key] = $value;
-                }
-                $teller++;
-            }
-            $fixed[] = $fixedRow;
-        }
-
-        return $fixed;
     }
 }
